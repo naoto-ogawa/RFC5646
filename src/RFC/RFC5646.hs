771ea7be65200e158b7d70d630121b937e6af7b2
naoto-ogawa <becausethespiderspiedher@gmail.com>
2018-05-27 14:47:02 +0900

    move to Internal and expose.

--- src/RFC/RFC5646.hs
+++ src/RFC/RFC5646.hs
@@ -1,244 +1,173 @@
+{-|
+Module      : RFC5646
+Description : the parser of RFC5646
+Copyright   : (c) Naoto 0gawa 2018-
+License     : MIT 
+Maintainer  : Naoto Ogawa 
+Stability   : experimental
+Portability : POSIX
+
+The parser of RFC5646
+https://tools.ietf.org/html/rfc5646
+
+Features :
+ Lens-enabled
+ instance of Read class
+   use readMaby
+ instance of Eq class
+   case insensitive equality
+ support for common convention formatting
+ check if a tag is valid
+ check if a tag is private
+
+Basic usage :
+
+>>> read "de" :: LanguageTag
+Normal (Langtag {_language = ISO639 {_primary = "de", _extended = []}, _script = Nothing, _region = Nothing, _variant = [], _extension = [], _privateuse = []})
+
+>>> read "DE" :: LanguageTag
+Normal (Langtag {_language = ISO639 {_primary = "DE", _extended = []}, _script = Nothing, _region = Nothing, _variant = [], _extension = [], _privateuse = []})
+
+Equality :
+
+>>> let l1 =read "de" :: LanguageTag
+>>> let l2 =read "DE" :: LanguageTag
+>>> l1 == l2
+True
+
+Tag Access : 
+
+zh-Hant (Chinese written using the Traditional Chinese script)
+
+>>> let c1 = read "zh-Hant" :: LanguageTag
+>>> c1 ^. (_Normal . language . primary)
+"zh"
+>>> c1 ^. (_Normal . language . extended)
+[]
+>>> c1 ^. (_Normal . script)
+Just "Hant"
+>>> c1 ^. (_Normal . region)
+Nothing
+>>> c1 ^. (_Normal . variant)
+[]
+>>> c1 ^. (_Normal . extension)
+[]
+>>> c1 ^. (_Normal . privateuse )
+[]
+
+zh-cmn-Hans-CN (Chinese, Mandarin, Simplified script, as used in China)
+
+>>> let c2 = read "zh-cmn-Hans-CN" :: LanguageTag
+>>> c2 ^. (_Normal . language . primary)
+"zh"
+>>> c2 ^. (_Normal . language . extended)
+["cmn"]
+>>> c2 ^. (_Normal . script)
+Just "Hans"
+>>> c2 ^. (_Normal . region)
+Just "CN"
+>>> c2 ^. (_Normal . variant)
+[]
+>>> c2 ^. (_Normal . extension)
+[]
+>>> c2 ^. (_Normal . privateuse)
+[]
+
+sl-rozaj-biske (San Giorgio dialect of Resian dialect of Slovenian)
+
+>>> let c3 = read "sl-rozaj-biske" :: LanguageTag
+>>> c3 ^. (_Normal . language . primary)
+"sl"
+>>> c3 ^. (_Normal . language . extended)
+[]
+>>> c3 ^. (_Normal . script)
+Nothing
+>>> c3 ^. (_Normal . region)
+Nothing
+>>> c3 ^. (_Normal . variant)
+["rozaj","biske"]
+>>> c3 ^. (_Normal . extension)
+[]
+>>> c3 ^. (_Normal . privateuse)
+[]
+
+az-Arab-x-AZE-derbend (Private use subtags)
+
+>>> let c4 = read "az-Arab-x-AZE-derbend" :: LanguageTag
+>>> c4
+Normal (Langtag {_language = ISO639 {_primary = "az", _extended = []}, _script = Just "Arab", _region = Nothing, _variant = [], _extension = [], _privateuse = ["x","AZE","derbend"]})
+>>> c4 ^. (_Normal . language . primary)
+"az"
+>>> c4 ^. (_Normal . language . extended)
+[]
+>>> c4 ^. (_Normal . script)
+Just "Arab"
+>>> c4 ^. (_Normal . region)
+Nothing
+>>> c4 ^. (_Normal . variant)
+[]
+>>> c4 ^. (_Normal . extension)
+[]
+>>> c4 ^. (_Normal . privateuse)
+["x","AZE","derbend"]
+
+Privateness : 
+
+qaa-Qaaa-QM-x-southern (all private tags)
+
+>>> let c5 = read "qaa-Qaaa-QM-x-southern" :: LanguageTag
+>>> c5 & previews _Normal isPrivateOfPrimaryLanguageSubtag
+Just True
+>>> c5 & previews _Normal isPrivateOfScript
+Just True
+>>> c5 & previews _Normal isPrivateOfPrimaryLanguageSubtag
+Just True
+
+Shortcut :
+
+When you know that you don't use a grandfathered tag  not a private tag at the beginning, use Langtag type insted of LanguageTag.
+
+>>> let c2' = read "zh-cmn-Hans-CN" :: LanguageTag
+>>> c2' ^. (_Normal . language . primary)
+"zh"
+>>> c2' ^. (_Normal . language . extended)
+["cmn"]
+>>> c2' ^. (_Normal . script)
+Just "Hans"
+>>> c2' ^. (_Normal . region)
+Just "CN"
+
+Safty :
+
+The read function is not safe, so use readMaybe
+
+>>> import Text.Read
+>>> readMaybe "qaa-Qaaa-QM-x-southern" :: Maybe LanguageTag
+Just (Normal (Langtag {_language = ISO639 {_primary = "qaa", _extended = []}, _script = Just "Qaaa", _region = Just "QM", _variant = [], _extension = [], _privateuse = ["x","southern"]}))
+
+>>> readMaybe "abcdefg" :: Maybe LanguageTag
+Nothing
+
+-}
+
 {-# LANGUAGE
     OverloadedStrings
+  , TemplateHaskell
+  , ExplicitForAll 
 #-}
 
 module RFC.RFC5646 (
-    Language(..)
+    LanguageTag(..)
+  , Language(..)
   , Langtag(..)
-  , pLangtag
-  , range
-  , pVariant
-  , pExtension 
-  , pSingleton
-  , pPrivateuse
-  , _pPrivateuse
-  , _pAlpha
+  , isPrivateOfPrimaryLanguageSubtag 
+  -- | uniquness
+  , isDuplicate
+  , isDuplicateVariant
+  , isDuplicateExtension
 ) where 
 
+import RFC.Internal.RFC5646
 import Control.Applicative
-import Data.Attoparsec.Combinator (lookAhead)
-import Data.Attoparsec.ByteString.Char8
-import qualified Data.Attoparsec.Internal.Types as T
+import Control.Lens
 
-data Langtag = Langtag {
-    _language   :: Language
-  , _script     :: Maybe String
-  , _region     :: Maybe String
-  , _variant    :: [String]
-  , _extension  :: [String]
-  , _privateuse :: [String] 
-  } deriving (Show, Eq)
-
-data Language = 
-  ISO639 String [String] |
-  Resv String            |
-  LangSubtag String
-  deriving (Show, Eq)
-
--- |  Language-Tag  = langtag             ; normal language tags
--- |                / privateuse          ; private use tag
--- |                / grandfathered       ; grandfathered tags
--- pLanguageTag = pLangtag <|> pPrivateuse <|> pGrandfathered  TODO
-
--- |  langtag       = language
--- |                  ["-" script]
--- |                  ["-" region]
--- |                  *("-" variant)
--- |                  *("-" extension)
--- |                  ["-" privateuse]
--- | 
-pLangtag :: Parser Langtag
-pLangtag = do
-  lang <- pLanguage
-  scri <- option "" (char '-' >> pScirpt    )
-  regi <- option "" (char '-' >> pRegion    )
-  vari <- many'     (char '-' >> pVariant   )
-  extn <- option [] (many' (char '-' >> pExtension))
-  priv <- option [] (char '-' >> pPrivateuse)
-  return $ Langtag lang (may scri) (may regi) vari extn priv
---  return $ Langtag lang (may scri) (may regi) vari extn Nothing 
---  return $ Langtag lang (may scri) (may regi) vari [] Nothing 
---  return $ Langtag lang (may scri) (may regi) [] [] Nothing 
---  return $ Langtag lang (may scri) Nothing [] [] Nothing 
---  return $ Langtag lang Nothing  Nothing [] [] Nothing 
-  where may x = if null x then Nothing else Just x
-
--- |  language      = 2*3ALPHA            ; shortest ISO 639 code
--- |                  ["-" extlang]       ; sometimes followed by
--- |                                      ; extended language subtags
--- |                / 4ALPHA              ; or reserved for future use
--- |                / 5*8ALPHA            ; or registered language subtag
--- | 
-pLanguage :: Parser Language
-pLanguage = iso639 <|> reserved <|> langsubtag 
-  where 
-    iso639 = do
-      x <- range 2 3 _pAlpha 
-      y <- option [] pExtlang 
-      return $ ISO639 x y 
-    reserved   = do
-      x <- count 4 _pAlpha
-      return $ Resv x
-    langsubtag = do
-      x <- range 5 8 _pAlpha
-      return $ LangSubtag x
-
--- |  extlang       = 3ALPHA              ; selected ISO 639 codes
--- |                  *2("-" 3ALPHA)      ; permanently reserved
--- pExtlang = (char '-' >> count 3 _pAlpha >>= \x -> return [x]) --  <|> resv)
-pExtlang = range 1 3 (char '-' >> iso639) --  <|> resv)
-  where 
-   iso639 = do
-     x <- count 3 _pAlpha
-     z <- peekChar
-     case z of
-       Just y  -> if y == '-' then return x else fail "not extlang"
-       Nothing -> return x 
-
--- |  script        = 4ALPHA              ; ISO 15924 code
-pScirpt = do 
-  x <- count 4 _pAlpha
-  z <- peekChar
-  case z of
-    Just y  -> if y == '-' then return x else fail "not pScript"
-    Nothing -> return x 
-
--- |  region        = 2ALPHA              ; ISO 3166-1 code
--- |                / 3DIGIT              ; UN M.49 code
-pRegion = do
-  x <- count 2 _pAlpha <|> count 3 digit 
-  z <- peekChar
-  case z of
-    Just y  -> if y == '-' then return x else fail "not pScript"
-    Nothing -> return x 
-
--- |  variant       = 5*8alphanum         ; registered variants
--- |                / (DIGIT 3alphanum)
--- pVariant = anyChar >> return "aaaaa"
-pVariant = do
-  x <- (vari1  <|> vari2)
-  z <- peekChar
-  case z of
-    Just y  -> if y == '-' then return x else fail "not pVariant"
-    Nothing -> return x 
-  where 
-    vari1 = range 5 8 pAlphanum 
-    vari2 = do 
-      d <- digit
-      x <- count 3 pAlphanum
-      return $ d : x
-
-
--- | extension     = singleton 1*("-" (2*8alphanum))
-pExtension :: Parser String
-pExtension = pSingleton >> char '-' >> range 2 8 pAlphanum
-
--- |
--- |                                     ; Single alphanumerics
--- |                                     ; "x" reserved for private use
--- | singleton     = DIGIT               ; 0 - 9
--- |               / %x41-57             ; A - W
--- |               / %x59-5A             ; Y - Z
--- |               / %x61-77             ; a - w
--- |               / %x79-7A             ; y - z
-pSingleton :: Parser Char
-pSingleton = 
-  digit                                <|>
-  satisfy (\c -> c >= 'A' && c <= 'W') <|>
-  satisfy (\c -> c >= 'Y' && c <= 'Z') <|>
-  satisfy (\c -> c >= 'a' && c <= 'w') <|>
-  satisfy (\c -> c >= 'y' && c <= 'z')
-
--- | alphanum      = (ALPHA / DIGIT)     ; letters and numbers
-pAlphanum :: Parser Char
-pAlphanum = _pAlpha <|> digit
-
-_pAlpha = satisfy isAlpha_ascii 
-
--- | privateuse    = "x" 1*("-" (1*8alphanum))
-pPrivateuse :: Parser [String]
-pPrivateuse = do 
-  char 'x' 
-  x <- many1 _pPrivateuse
-  return x
-
-_pPrivateuse :: Parser [Char]
-_pPrivateuse = do
-  char '-' 
-  x <- range 1 8 pAlphanum 
-  return x
-
--- | an equivalence of regrex {min, max}
-range :: Int -> Int -> T.Parser i a -> T.Parser i [a]
-range n m p = (++) 
-              <$> count n p       -- mimimam
-              <*> upto (m - n) p  -- rest
-
--- | until n times.
-upto :: Int -> T.Parser i a -> T.Parser i [a]
-upto 0 _ = return []
-upto n p = (:) <$> 
-               p                -- at least once
-           <*> upto (n - 1) p   -- repeat
-               <|> return []    -- in case of faliure
-
--- | grandfathered = irregular           ; non-redundant tags registered
--- |                / regular             ; during the RFC 3066 era
-pGrandfathered = pIrregular <|> pRegular
-
--- | irregular     = "en-GB-oed"         ; irregular tags do not match
--- |               / "i-ami"             ; the 'langtag' production and
--- |               / "i-bnn"             ; would not otherwise be
--- |               / "i-default"         ; considered 'well-formed'
--- |               / "i-enochian"        ; These tags are all valid,
--- |               / "i-hak"             ; but most are deprecated
--- |               / "i-klingon"         ; in favor of more modern
--- |               / "i-lux"             ; subtags or subtag
--- |               / "i-mingo"           ; combination
--- |               / "i-navajo"
--- |               / "i-pwn"
--- |               / "i-tao"
--- |               / "i-tay"
--- |               / "i-tsu"
--- |               / "sgn-BE-FR"
--- |               / "sgn-BE-NL"
--- |               / "sgn-CH-DE"
-
-pIrregular = 
-  string "en-GB-oed" <|>
-  string "i-ami"     <|>
-  string "i-bnn"     <|>
-  string "i-default" <|>
-  string "i-enochian" <|>
-  string "i-hak"     <|>
-  string "i-klingon" <|>
-  string "i-lux"     <|>
-  string "i-mingo"   <|>
-  string "i-navajo" <|>
-  string "i-pwn" <|>
-  string "i-tao" <|>
-  string "i-tay" <|>
-  string "i-tsu" <|>
-  string "sgn-BE-FR" <|>
-  string "sgn-BE-NL" <|>
-  string "sgn-CH-DE"
-
--- | regular       = "art-lojban"        ; these tags match the 'langtag'
--- |               / "cel-gaulish"       ; production, but their subtags
--- |               / "no-bok"            ; are not extended language
--- |               / "no-nyn"            ; or variant subtags: their meaning
--- |               / "zh-guoyu"          ; is defined by their registration
--- |               / "zh-hakka"          ; and all of these are deprecated
--- |               / "zh-min"            ; in favor of a more modern
--- |               / "zh-min-nan"        ; subtag or sequence of subtags
--- |               / "zh-xiang"<Paste>
-pRegular = 
-  string "art-lojban"        <|>
-  string "cel-gaulish"       <|>
-  string "no-bok"            <|>
-  string "no-nyn"            <|>
-  string "zh-guoyu"          <|>
-  string "zh-hakka"          <|>
-  string "zh-min"            <|>
-  string "zh-min-nan"        <|>
-  string "zh-xiang"
